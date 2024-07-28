cf275a928a139c69db6f266ceddc59d88f5112a6
79206 <serdukovd.a.murom@yandex.ru>
2024-07-28 11:42:43 +0300

    star task

---
+++ collections_list/src/main/java/pro/skyp/collections_list/CollectionsListApplication.java
@@ -0,0 +1,13 @@
+package pro.skyp.collections_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsListApplication.class, args);
+	}
+
+}
